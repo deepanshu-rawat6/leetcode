c93921bfd81f61d6514667c0e84bbf207c96d25a
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-02 00:10:07 +0530

    O(N*logN) approach

---
+++ Maximum Product of Three Numbers/sol.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len=nums.length;
+        return Math.max((nums[len-1]*nums[len-2]*nums[len-3]),(nums[0]*nums[1]*nums[len-1]));
+    }
+}
