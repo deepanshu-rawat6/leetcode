3e6d93ce799104c6c0353c4be76d472bfdd41666
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-09 23:05:21 +0530

    solved using array manipulation

---
+++ Plus One/sol.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len=digits.length;
+        for(int i=len-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] ans=new int[len+1];
+        ans[0]=1;
+        return ans;
+    }
+}
