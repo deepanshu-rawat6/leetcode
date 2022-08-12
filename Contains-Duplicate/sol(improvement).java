b08a78acd70c1fe7bb5442420f7dfd1aba841495
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-13 01:39:52 +0530

    adding improvement solution

---
+++ Contains-Duplicate/sol(improvement).java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> ans=new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(ans.contains(nums[i])){
+                return true;
+            }
+            ans.add(nums[i]);
+        }
+        return false;
+    }
+}
