894c9ab6e6e41a73aa2a50c82fc93644c7f727d7
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-12-15 18:03:12 +0530

    Solved: is Subsequence using Two Pointers and DP

---
+++ Is Subsequence/Solution.java
@@ -0,0 +1,49 @@
+/*
+Two Pointers approach: 
+TC: O(max(m,n))
+SC: O(1)
+*/
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int i = 0,j = 0;
+        while (i < m && j < n) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == m;
+    }
+}
+
+
+/*
+DP approach, same as LCS problem, we just compare the value of LCS 
+obtained with the length of the string to be compared
+
+TC: O(m*n)
+SC: O(m*n)
+*/
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        // m = s.length(), n = t.length()
+        int[][] DP = new int[s.length() + 1][t.length() + 1]; 
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= t.length(); j++) {
+                if (i == 0 || j == 0) {
+                    DP[i][j] = 0;
+                } else if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    DP[i][j] = DP[i - 1][j - 1] + 1;
+                } else {
+                    DP[i][j] = Math.max(DP[i - 1][j],DP[i][j - 1]);
+                }
+            }
+        }
+        return s.length() == DP[s.length()][t.length()];
+    }
+}
+
---
+++ Is Subsequence/readme.md
@@ -0,0 +1,3 @@
+Link to the solution: https://leetcode.com/problems/is-subsequence/description/
+
+Solution submitted: https://leetcode.com/problems/is-subsequence/solutions/2914504/java-solution-100-faster-92-12-efficient/
